ddbbc08f01c03b314c414770c11a01f6374d12f3
Vinay Kumar Khare <38154496+vinaykhare@users.noreply.github.com>
2022-06-03 01:02:03 +0530

    yes created new directory with same code

--- flutter/lib/apps/chat/chat_message.dart
+++
@@ -1,9 +0,0 @@
-class ChatMessage {
-  String content;
-  String from;
-  ChatMessage({required this.content, required this.from});
-  Map toJson() => {'content': content, 'from': from};
-  factory ChatMessage.fromJson(dynamic json) {
-    return ChatMessage(from: json['from'], content: json['content']);
-  }
-}
--- flutter/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:apnavaio/apna_vaio.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const ApnaVaio());
-}
